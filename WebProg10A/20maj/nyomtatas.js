3096dc2dd7e933a6b6faa8b962dd4f3a9ef80ff0
Zan <mestermc594@gmail.com>
2023-10-11 12:31:49 +0200

    202310111231

--- WebProg10A/20maj/nyomtatas.js
+++ WebProg10A/20maj/nyomtatas.js
@@ -7,6 +7,8 @@ function kalkulal(){
     let terulet=Math.round((szelesseg*magassag)/10000);    
     let koltseg=terulet*papir;
     //Megjelenítés
+    document.getElementById('terulet').textContent = terulet;
+    document.getElementById('papir').textContent = papir;
+    document.getElementById('koltseg').textContent = koltseg;
     document.getElementById('valasz').style.visibility = "visible";
 }
-
