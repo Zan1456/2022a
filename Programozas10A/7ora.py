a4238d1f180860f0d05a083c5bcee726e5a2d07e
Zan1456 <mestermc594@gmail.com>
2023-11-13 10:45:06 +0100

    Update 7ora.py

--- Programozas10A/7ora.py
+++ Programozas10A/7ora.py
@@ -1,14 +1,25 @@
+'''
 def fejlec(cim):
     szelesseg = 30
     csillagok = "*"*szelesseg
-    kozepso_csillag = "*"*((szelesseg-len(cim))//2)
+    kozepso_csillag = "*"*(((szelesseg-len(cim))//2)-1)
     print(csillagok)
     if len(cim) % 2 == 0:
-        print(kozepso_csillag+cim+kozepso_csillag)
+        print(kozepso_csillag+" "+cim+" "+kozepso_csillag)
     else:
-        print(kozepso_csillag+cim+kozepso_csillag+"*")
+        print(kozepso_csillag+" "+cim+" "+kozepso_csillag+"*")
     print(csillagok)
 teszt=""
 for i in range(2,20):
     teszt+="C"
     fejlec(teszt)
+'''
+x=11_222_231
+print(x+1)
+
+print(0o123) #8-as számrendszer
+print(0xABBA) #16-os számrendszer
+
+x=43
+# 16-os számrendszerbe váltás
+y="0x"+x
