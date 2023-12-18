67ab3a4d2008e1a008a68fe017c24074bd2eeab2
Zan1456 <mestermc594@gmail.com>
2023-12-18 11:42:01 +0100

    s

--- Programozas10A/12ora/12ora.py
+++ Programozas10A/12ora/12ora.py
@@ -1,3 +1,4 @@
+from collections import Counter
 # Adatok:
 #   Fájlban:    Dolgozók nevei szerepelnek
 #               Mellette hogy havonta mennyi palacsintát sütöttek meg
@@ -6,7 +7,7 @@
 
 filename="input.txt"
 
-file=open(filename, "r")
+file=open(filename, "r", encoding="utf-8")
 file_lista=file.readlines()
 file.close()
 
@@ -61,20 +62,27 @@ for i in range(len(name_list)):
 # 6) Volt-e olyan személy, akinek az évben változó teljesítménye volt?
 #       Pl: sütött valamennyit, majd a következő hónapban többet, a következőben az előzőnél kevesebbet stb
 #       írd ki a személy nevét!
-van = False
+elemzes = []
 for i in range(len(pancake_list)):
-    ossz = pancake_list[i][0]
-    for j in range(11):
-        if ossz < pancake_list[i][j] and pancake_list[i][j] > pancake_list[i][j+1]:
-            van = True
-            break
-print(van)
+    lista = []
+    for j in range(10):
+        if pancake_list[i][j] < pancake_list[i][j+1] and pancake_list[i][j+1] > pancake_list[i][j+2]:
+            liste = 0
+        elif pancake_list[i][j] < pancake_list[i][j+1] and pancake_list[i][j+1] < pancake_list[i][j+2]:
+            liste = 1
+        elif pancake_list[i][j] > pancake_list[i][j+1] and pancake_list[i][j+1] > pancake_list[i][j+2]:
+            liste = 2
+        lista.append(liste)
+    elemzes.append(lista)
 
+def most_frequent(List):
+    return max(set(List), key = List.count)
 
-
-
-
-
-
-
-
+for i in range(len(elemzes)):
+    esd = most_frequent(elemzes[i])
+    if esd == 0:
+        print(name_list[i] + " Változó")
+    elif esd == 1:
+        print("Növekvő")
+    else:
+        print("Csökkenő")
