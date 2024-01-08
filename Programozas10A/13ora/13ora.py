7d40a3d709c6c4ae846d42c69958cccf4d3b03c5
Zan1456 <mestermc594@gmail.com>
2024-01-08 10:22:05 +0100

    Create 13ora.py

---
+++ Programozas10A/13ora/13ora.py
@@ -0,0 +1,4 @@
+# 1. Készíts egy programot, amely bekéri egy diákok százalékos eredményeit, majd kiírja a legrosszabb és legjobb eredményt.
+# 2. Kérj be egy MINIMUM és MAXIMUM százalékos eredményt, a program, pedig írja ki, hogy hány és milyen eredmény van-e intervallumban.
+# 3. Számoljuk ki az osztályátlagot! Az átlag számolás bármilyen hosszú listán működjön.
+# 4. Csináljunk egy másik listát azonos elemszámmal az elsővel és töltsd fel random számokkal 0-100 intervallumon belül. Majd írjuk ki melyik osztály átlaga nagyobb.
