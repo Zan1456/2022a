9579e32293ddb910feb54520bc24458fccf0530e
Zan1456 <mestermc594@gmail.com>
2024-01-24 11:44:09 +0100

    Webprog updates

---
+++ WebProg10A/Órák/12. óra/index.js
@@ -0,0 +1,14 @@
+// Alap parancsok
+console.log("Konzol üzenet");
+
+
+// Függvények
+
+function Gomb() {
+    document.getElementById("atiras").innerHTML = "Üzenet átírva";
+}
+
+function Ertesites() {
+    alert("Értesítés kiküldve");
+    // window.alert("Értesítés kiküldve")
+}
