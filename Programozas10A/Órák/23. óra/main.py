b253492c7b2fee55eb5b1d08e4eb3255dd713b9d
Zan1456 <mestermc594@gmail.com>
2024-04-22 11:25:30 +0200

    23. óra

---
+++ Programozas10A/Órák/23. óra/main.py
@@ -0,0 +1,54 @@
+# py main.py input1
+import sys
+
+def read(filename, first = 0, last = 0, nameloc = 0): # Inp1 es Inp2, fel2 ✔
+    file = open(filename, encoding="utf-8")
+    rows = file.readlines()
+    file.close()
+    data = []
+    for i in range(first, len(rows)-last):
+        data.append(rows[i].strip().split(","))
+    names = []
+    for i in range(len(data)):
+        names.append(data[i][nameloc])
+        data[i].pop(nameloc)
+    return names, data
+
+def maxi(lista): #inp1, fel3 ✔
+    maxx = int(lista[1][0][1])
+    maxi = 0
+    for i in range(len(lista[1])):
+        if maxx < int(lista[1][i][1]):
+            maxi = i
+            maxx = int(lista[1][i][1])
+            
+    return lista[0][i]
+
+def atlag(lista): #inp1, fel4 ✔
+    osszeg = 0
+    for i in range(len(lista[1])):
+        osszeg += int(lista[1][i][2])
+    got = []
+    for i in range(len(lista[1])):
+        if osszeg // len(lista[1]) <= int(lista[1][i][2]):
+            got.append(lista[0][i])
+    return got
+
+def osszatlag(lista): #inp1, fel5 es fel6 ✔
+    egesz = []
+    osszeg = 0
+    for i in range(len(lista[1])):
+        for j in range(len(lista[1][i])):
+            osszeg += int(lista[1][i][j])
+        egesz.append(osszeg // int(len(lista[1][i])))
+    return egesz
+
+# Input1 fájl
+input1=read(sys.argv[1], 1)
+print(maxi(input1))
+print(atlag(input1))
+print(osszatlag(input1))
+print(osszatlag(input1)[3])
+
+# Input2 fájl
+input2=read(sys.argv[2], 1, nameloc=12)
---
+++ Programozas10A/Órák/23. óra/readme.md
@@ -0,0 +1,19 @@
+# 2024.04.22. 23. óra
+
+## 1. feladat: Input1
+
+1. Terminálból fájlnév ✔
+1. Beolvasás fügvény -> fájlnév atribútummal ✔
+1. Kedden ki érte el a maximumot? ✔
+1. Szerdán ki érte el az aznapi átlagot? ✔
+1. Mennyi a számok átlaga (összes nap, személlyel)? ✔
+1. Mennyi Yennefer átlag napi teljesítménye? ✔
+
+## 2. feladat: Input2
+
+1. Terminálból fájlnév ✔
+1. Beolvasás fügvény -> fájlnév atribútummal ✔
+1. Márciusban és Júnuisban összesítve ki volt a legügyesebb?
+1. Mennyi Zita éves átlaga?
+1. Annak lett az eredménye februárban 5db, ha tudjuk, hogy volt ilyen személy
+1. A márciusi átlag felfelé kerekítve értékénél volt-e rosszabban teljesített szeptemberben? Ha igen, ki?
